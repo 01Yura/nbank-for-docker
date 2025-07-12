10f8a4122e3ff6cf8ac749dbf3ad4adc139aa808
Yuri Primyshev <p020885@yandex.ru>
2025-07-12 00:35:21 -0400

    add ui tests, iteration1_junior_level

---
+++ src/test/java/ui/iteration1_junior_level/CreateAccountTest.java
@@ -0,0 +1,97 @@
+package ui.iteration1_junior_level;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import io.restassured.common.mapper.TypeRef;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import senior.models.CreateAccountResponseModel;
+import senior.models.CreateUserRequestModel;
+import senior.models.LoginUserRequestModel;
+import senior.requests.skeleton.requesters.CrudRequester;
+import senior.requests.skeleton.requesters.Endpoint;
+import senior.requests.steps.AdminSteps;
+import senior.specs.RequestSpecs;
+import senior.specs.ResponseSpecs;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CreateAccountTest {
+    @BeforeAll
+    static void setupSelenoid() {
+//        Configuration.remote = "http://192.168.0.127:4444/wd/hub";
+        Configuration.baseUrl = "http://192.168.0.22:3000";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", true,
+                        "enableLog", true,
+                        "enableVideo", true)
+        );
+    }
+
+    @Test
+    void userCanCreateAccount() {
+//        STEPS TO SET UP ENVIRONMENT: (делаются на уровне API)
+//        1. Admin login
+//        2. Admin create user
+//        3. User login
+
+        CreateUserRequestModel user = AdminSteps.createUser();
+
+        String userAuthToken = new CrudRequester(RequestSpecs.unauthSpec(),
+                ResponseSpecs.responseReturns200Spec(),
+                Endpoint.AUTH_LOGIN)
+                .post(LoginUserRequestModel.builder().username(user.getUsername()).password(user.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        Selenide.open("/");
+
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthToken);
+
+        Selenide.open("/dashboard");
+
+//        TEST STEPS:
+//        4. User creates account
+        $(Selectors.byText("➕ Create New Account")).click();
+
+//        Account has been created on UI
+        Alert alert = switchTo().alert();
+        assertThat(alert.getText()).contains("✅ New Account Created! Account Number:");
+        String alertText = alert.getText();
+
+        Pattern pattern = Pattern.compile("Account Number: (\\w+)");
+        Matcher matcher = pattern.matcher(alertText);
+        matcher.find();
+        String accountNumber = matcher.group(1);
+
+//        Account has been created on API
+        List<CreateAccountResponseModel> existingUserAccounts = given()
+                .spec(RequestSpecs.userSpec(user.getUsername(), user.getPassword()))
+                .when()
+                .get("http://localhost:4111/api/v1/customer/accounts")
+                .then()
+                .assertThat()
+                .extract().as(new TypeRef<List<CreateAccountResponseModel>>() {
+                });
+
+        assertThat(existingUserAccounts).hasSize(1);
+
+        CreateAccountResponseModel createdAccount = existingUserAccounts.getFirst();
+
+        assertThat(createdAccount).isNotNull();
+        assertThat(createdAccount.getBalance()).isZero();
+    }
+}
---
+++ src/test/java/ui/iteration1_junior_level/CreateUserTest.java
@@ -0,0 +1,137 @@
+package ui.iteration1_junior_level;
+
+import com.codeborne.selenide.*;
+import io.restassured.common.mapper.TypeRef;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import senior.generators.RandomModelGenerator;
+import senior.models.CreateUserRequestModel;
+import senior.models.CreateUserResponseModel;
+import senior.models.comparison.ModelAssertions;
+import senior.specs.RequestSpecs;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CreateUserTest {
+    @BeforeAll
+    static void setupSelenoid() {
+//        Configuration.remote = "http://192.168.0.127:4444/wd/hub";
+        Configuration.baseUrl = "http://192.168.0.22:3000";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", true,
+                        "enableLog", true,
+                        "enableVideo", true)
+        );
+    }
+
+    @Test
+    void adminCanCreateUserWithValidCredentials() {
+        //        Login as admin
+        CreateUserRequestModel admin = CreateUserRequestModel.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        Selenide.open("/login");
+
+        $(Selectors.byAttribute("placeholder", "Username")).sendKeys(admin.getUsername());
+        $(Selectors.byAttribute("placeholder", "Password")).sendKeys(admin.getPassword());
+        $("button").click();
+
+        $(Selectors.byText("Admin Panel")).shouldBe(Condition.visible);
+
+//        Create user
+        CreateUserRequestModel user =
+                RandomModelGenerator.generateRandomModel(CreateUserRequestModel.class);
+
+        $(Selectors.byAttribute("placeholder", "Username")).sendKeys(user.getUsername());
+        $(Selectors.byAttribute("placeholder", "Password")).sendKeys(user.getPassword());
+        $(Selectors.byText("Add User")).click();
+
+//        Check that alert equals expected alert
+        Alert alert = switchTo().alert();
+        assertThat(alert.getText()).isEqualTo("✅ User created successfully!");
+        alert.accept();
+
+//        Check that user is shown on UI
+        ElementsCollection allUsers = $(Selectors.byText("All Users")).parent().findAll("li");
+        allUsers.findBy(Condition.partialText(user.getUsername())).shouldBe(Condition.visible);
+
+//        Check that user was created on API
+        List<CreateUserResponseModel> users = given()
+                .spec(RequestSpecs.adminSpec())
+                .when()
+                .get("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .statusCode(200)
+                .extract().as(new TypeRef<List<CreateUserResponseModel>>() {
+                });
+
+        CreateUserResponseModel createdUser =
+                users.stream().filter(u -> u.getUsername().equals(user.getUsername())).findFirst().get();
+
+        ModelAssertions.assertThatModels(user, createdUser).match();
+    }
+
+    @Test
+    void adminCannotCreateUserWithInvalidCredentials() {
+//        Login as admin
+        CreateUserRequestModel admin = CreateUserRequestModel.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        Selenide.open("/login");
+
+        $(Selectors.byAttribute("placeholder", "Username")).sendKeys(admin.getUsername());
+        $(Selectors.byAttribute("placeholder", "Password")).sendKeys(admin.getPassword());
+        $("button").click();
+
+        $(Selectors.byText("Admin Panel")).shouldBe(Condition.visible);
+
+//        Create user
+        CreateUserRequestModel user =
+                RandomModelGenerator.generateRandomModel(CreateUserRequestModel.class);
+
+        user.setUsername("a"); // меняем username на invalid
+
+        $(Selectors.byAttribute("placeholder", "Username")).sendKeys(user.getUsername());
+        $(Selectors.byAttribute("placeholder", "Password")).sendKeys(user.getPassword());
+        $(Selectors.byText("Add User")).click();
+
+//        Check that alert equals expected alert
+        Alert alert = switchTo().alert();
+        assertThat(alert.getText()).contains("Failed to create user");
+        alert.accept();
+
+//        Check that the user is not shown on UI
+        ElementsCollection allUsers = $(Selectors.byText("All Users")).parent().findAll("li");
+        allUsers.findBy(Condition.exactText(user.getUsername() + "\nUSER")).shouldNotBe(Condition.exist);
+
+//        Check that the user was not created on API
+        List<CreateUserResponseModel> users = given()
+                .spec(RequestSpecs.adminSpec())
+                .when()
+                .get("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .statusCode(200)
+                .extract().as(new TypeRef<List<CreateUserResponseModel>>() {
+                });
+
+        long numberOfUsersWithSameUsernameAsCreatedUser =
+                users.stream().filter(u -> u.getUsername().equals(user.getUsername())).count();
+
+        assertThat(numberOfUsersWithSameUsernameAsCreatedUser).isZero();
+    }
+}
