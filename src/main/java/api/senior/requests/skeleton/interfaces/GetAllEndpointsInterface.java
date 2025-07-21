9ec71ef454a05e42bdad4ded9de85331ef1ec108
Yuri Primyshev <p020885@yandex.ru>
2025-07-21 11:03:51 -0400

    added about getAll

--- src/main/java/api/senior/requests/skeleton/interfaces/GetAllEndpointsInterface.java
+++ src/main/java/api/senior/requests/skeleton/interfaces/GetAllEndpointsInterface.java
@@ -1,5 +1,7 @@
 package api.senior.requests.skeleton.interfaces;
 
+//      Метод getAll используется для получения списка всех объектов определённого типа
+//      (например, всех пользователей, всех аккаунтов и т.д.) с сервера через API.
 public interface GetAllEndpointsInterface {
     Object getAll(Class<?> clazz);
 }
