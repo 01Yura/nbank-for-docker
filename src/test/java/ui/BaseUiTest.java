885aa8a0c3047f8d27d14c8103bc5ff46daba9af
Yuri Primyshev <p020885@yandex.ru>
2025-08-07 01:19:22 -0400

    added Telegramm bot in docker-image-push pipeline

--- src/test/java/ui/BaseUiTest.java
+++ src/test/java/ui/BaseUiTest.java
@@ -21,8 +21,6 @@ import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 
-// тест пайплайна
-
 @ExtendWith(AdminSessionExtension.class)
 @ExtendWith(UserSessionExtension.class)
 @ExtendWith(BrowserMatchExtension.class)
