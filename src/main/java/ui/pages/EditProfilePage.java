aabf4a6c138ef7849189c16cbfb349eb4c4a65f4
Yuri Primyshev <p020885@yandex.ru>
2025-08-13 21:22:36 -0400

    add some changes

--- src/main/java/ui/pages/EditProfilePage.java
+++ src/main/java/ui/pages/EditProfilePage.java
@@ -19,11 +19,6 @@ public class EditProfilePage extends BasePage<EditProfilePage> {
 
     public EditProfilePage changeName(String name) {
         enterNewNameInput.sendKeys(name);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         saveChangesButton.click();
         return this;
     }
