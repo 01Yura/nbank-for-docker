001faba82d0db89ec80c57437e5e595cdeb9e7f5
Yuri Primyshev <p020885@yandex.ru>
2025-10-19 15:29:02 -0400

    Update CI/CD pipeline comment in BaseUiTest
    Adjusted the comment in BaseUiTest to clarify its purpose with a version marker. This change ensures consistency and tracks updates in pipeline-related tests.

--- src/test/java/ui/BaseUiTest.java
+++ src/test/java/ui/BaseUiTest.java
@@ -24,7 +24,7 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 @ExtendWith(UserSessionExtension.class)
 @ExtendWith(BrowserMatchExtension.class)
 
-// проверка CI/CD pipeline
+// проверка CI/CD pipeline 1
 
 public class BaseUiTest extends BaseApiTest {
     @BeforeAll
